3c97341f18da8a292b0baa60a953e14393e56bc4
lishuai <nvidia@yeah.net>
2020-08-30 21:53:14 +0800

    commit_message

---
+++ demo1/src/main/java/com/lishuai/spring/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.lishuai.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lishuai
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
