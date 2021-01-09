1d5e5b39f29bb8ef9449f2497300bbb2af2f7ce0
lishuai <754268653@qq.com>
2021-01-09 15:57:41 +0800

    增加word、pdf代码

--- demo1/src/main/java/com/lishuai/spring/controller/WordsTest.java
+++ demo1/src/main/java/com/lishuai/spring/controller/WordsTest.java
@@ -9,6 +9,17 @@ import java.io.InputStream;
 public class WordsTest {
     /**
      * maven打包时，需要把模板文件取消打包，要不然会有进制问题
+     * 同时，服务器若没有字体，会有乱码问题，把Windows电脑上的字体上传到服务器上的 usr/share/fonts下（没有就创建）
+     * # 使mkfontscale和mkfontdir命令正常运行
+     * yum install mkfontscale
+     * # 使fc-cache命令正常运行。若是提示 fc-cache: command not found
+     * yum install fontconfig
+     *
+     * 使用命令
+     * mkfontscale
+     * mkfontdir
+     * fc-cache -fv
+     * 即可
      * @param args
      */
     public static void main(String[] args) {
